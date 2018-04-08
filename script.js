60c7eb0e8455d214842a27013d43580a25f0f751
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-04-07 20:28:29 -0400

    Added geojson

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+//console.log(parks.features);
+
+var mymap = L.map('mapid').setView([40.7531114, -73.9450298], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
     maxZoom: 18,
     id: 'mapbox.streets',
     accessToken: config.MY_KEY
 }).addTo(mymap);
+
+for (var i = 0; i < parks.features.length; i++) {
+
+  console.log(parks.features[i]);
+  L.geoJSON(parks.features[i]).addTo(mymap)
+
+}
