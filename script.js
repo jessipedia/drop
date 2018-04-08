c8bb0739d9914f14e915248886aa3fd39fb2850d
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2018-04-08 13:58:12 -0400

    Site ID and Fountain numbers to arrays

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-//console.log(parks.features);
+//console.log(parks.features.length);
+//Property Name,Property Number,Site Name,Site ID,Borough,Community Board,Drinking Fountains
+let fountains = "drinking_fountains.csv";
+let csvData;
+let siteId = [];
+let numFountains = [];
+readTextFile(fountains)
 
 var mymap = L.map('mapid').setView([40.7531114, -73.9450298], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -10,7 +16,34 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 
 for (var i = 0; i < parks.features.length; i++) {
 
-  console.log(parks.features[i]);
+  //console.log(parks.features[i].properties.omppropid);
   L.geoJSON(parks.features[i]).addTo(mymap)
 
+}
+
+function readTextFile(file)
+{
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, false);
+    rawFile.onreadystatechange = function ()
+    {
+        if(rawFile.readyState === 4)
+        {
+            if(rawFile.status === 200 || rawFile.status == 0)
+            {
+                var allText = rawFile.responseText;
+                csvData = allText.split('\n');
+                for (var i = 0; i < csvData.length; i++) {
+                  let row = csvData[i].split(',');
+                  siteId.push(row[3]);
+                  numFountains.push(row[6]);
+
+                }
+                //console.log(csvData);
+                console.log(siteId);
+                console.log(numFountains);
+            }
+        }
+    }
+    rawFile.send(null);
 }
